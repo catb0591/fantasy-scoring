f718f8083b44c8d762fb7153c2f0764f0dbe8a20
Cat <catb0591@gmail.com>
2021-11-06 15:20:13 -0400

    Cleaned up w/ Lint

--- scoring.js
+++ scoring.js
@@ -1,36 +1,33 @@
 const calculateScore = (player) => {
-
   switch (player.position)
-  { 
+  {
     case 'QB': return QBResults(player)
       break;
-      case 'RB': return RBResults(player)
+    case 'RB': return RBResults(player)
       break;
-      case 'WR': return WRResults(player)
+    case 'WR': return WRResults(player)
       break;
-      case 'TE': return TEResults(player)
+    case 'TE': return TEResults(player)
       break;
-      default: return 0
+    default: return 0
       break;
   }
-
 }
 
 const QBResults = (player) => {
-
-    const results = (player.stats.passing.yards / 25) +
+  const results = (player.stats.passing.yards / 25) +
         (player.stats.passing.touchdowns * 6) +
         (player.stats.passing.interceptions * -3) +
         (player.stats.rushing.yards / 10) +
         (player.stats.rushing.touchdowns * 6) +
         (player.stats.rushing.fumbles * -3)
 
-        return results
-    }
 
-    const RBResults = (player) => {
+  return results
+}
 
-        const results = (player.stats.rushing.yards / 10) +
+const RBResults = (player) => {
+  const results = (player.stats.rushing.yards / 10) +
         (player.stats.rushing.touchdowns * 6) +
         (player.stats.rushing.fumbles * -3) +
         (player.stats.receiving.receptions) +
@@ -44,11 +41,11 @@ const QBResults = (player) => {
         (player.stats.return.puntreturn.touchdowns * 6) +
         (player.stats.return.puntreturn.fumbles * -3)
 
-        return results
-    }
+  return results
+}
 
-    const WRResults = (player) => {
-        const results = (player.stats.rushing.yards / 10) +
+const WRResults = (player) => {
+  const results = (player.stats.rushing.yards / 10) +
         (player.stats.rushing.touchdowns * 6) +
         (player.stats.rushing.fumbles * -3) +
         (player.stats.receiving.receptions) +
@@ -62,18 +59,17 @@ const QBResults = (player) => {
         (player.stats.return.puntreturn.touchdowns * 6) +
         (player.stats.return.puntreturn.fumbles * -3)
 
-        return results
-    }
+  return results
+}
 
-    const TEResults = (player) => {
-
-        const results = (player.stats.receiving.receptions) +
+const TEResults = (player) => {
+  const results = (player.stats.receiving.receptions) +
         (player.stats.receiving.yards / 10) +
         (player.stats.receiving.touchdowns * 6) +
         (player.stats.receiving.fumbles * -3)
 
-        return results
-    }
+  return results
+}
 
 module.exports = calculateScore
 
